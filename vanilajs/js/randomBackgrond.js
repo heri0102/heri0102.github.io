605470bd3a6cd5ce15e7f2ffd5b1e8ee35d87498
heri0102 <81543639+heri0102@users.noreply.github.com>
2024-06-02 22:44:12 +0900

    Update randomBackgrond.js

--- vanilajs/js/randomBackgrond.js
+++ vanilajs/js/randomBackgrond.js
@@ -1,6 +1,4 @@
 const backgroundImg = ["01.jfif","02.jfif","03.jfif"];
   
 const chonsenImage = backgroundImg[Math.floor(Math.random() * backgroundImg.length)];
-document.body.style.backgroundImage = "/img/" + chonsenImage;
-  
-  
+document.body.style.backgroundImage = `img/${chonsenImage}`;
