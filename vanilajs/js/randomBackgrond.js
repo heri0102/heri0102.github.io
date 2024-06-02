96df5d9fbd81b962a3ca049ee4e758b33ba7f9ba
heri0102 <81543639+heri0102@users.noreply.github.com>
2024-06-03 05:35:08 +0900

    Delete vanilajs directory

--- vanilajs/js/randomBackgrond.js
+++
@@ -1,5 +0,0 @@
-const backgroundImg = ["01.jfif","02.jfif","03.jfif"];
-
-const chosenImage = backgroundImg[Math.floor(Math.random() * backgroundImg.length)];
-
-document.body.style.backgroundImage = `url('img/${chosenImage}')`;
